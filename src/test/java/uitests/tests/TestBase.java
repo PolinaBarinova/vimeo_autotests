627d05d0c57921f0e899252f0b27ad408877e546
Polina Barinova <barinova_polina@list.ru>
2025-08-02 18:18:06 +0300

    test fix of beforeAll

--- src/test/java/uitests/tests/TestBase.java
+++ src/test/java/uitests/tests/TestBase.java
@@ -8,6 +8,7 @@ import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.PageLoadStrategy;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
@@ -18,23 +19,51 @@ public class TestBase {
 
     @BeforeAll
     public static void setup() {
+//        Configuration.baseUrl = "https://vimeo.com/";
+//        Configuration.browser = System.getProperty("browser", "chrome");
+//        Configuration.browserSize = System.getProperty("browser.size", "1920x1080");
+//        Configuration.browserVersion = System.getProperty("browser.version", "128.0");
+//        Configuration.timeout = 10000;
+//        Configuration.remote = String.format(
+//                "https://%s:%s@%s/wd/hub",
+//                System.getProperty("selenoid.login", "user1"),
+//                System.getProperty("selenoid.password", "1234"),
+//                System.getProperty("selenoid.url", "selenoid.autotests.cloud")
+//        );
+//
+//        DesiredCapabilities capabilities = new DesiredCapabilities();
+//        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+//                "enableVNC", true,
+//                "enableVideo", true
+//        ));
+//
+//        ChromeOptions options = new ChromeOptions();
+//        options.addArguments(
+//                "--disable-blink-features=AutomationControlled",
+//                "--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36"
+//        );
+//        options.setExperimentalOption("excludeSwitches", List.of("enable-automation"));
+//        options.setExperimentalOption("useAutomationExtension", false);
+//
+//        capabilities.setCapability(ChromeOptions.CAPABILITY, options);
+//
+//        Configuration.browserCapabilities = capabilities;
+
         Configuration.baseUrl = "https://vimeo.com/";
         Configuration.browser = System.getProperty("browser", "chrome");
         Configuration.browserSize = System.getProperty("browser.size", "1920x1080");
-        Configuration.browserVersion = System.getProperty("browser.version", "128.0");
+        Configuration.browserVersion = System.getProperty("browser.version", "");
         Configuration.timeout = 10000;
-        Configuration.remote = String.format(
-                "https://%s:%s@%s/wd/hub",
-                System.getProperty("selenoid.login", "user1"),
-                System.getProperty("selenoid.password", "1234"),
-                System.getProperty("selenoid.url", "selenoid.autotests.cloud")
-        );
 
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
-                "enableVNC", true,
-                "enableVideo", true
-        ));
+        String remote = System.getProperty("selenoid.url", "");
+        if (!remote.isBlank()) {
+            Configuration.remote = String.format(
+                    "https://%s:%s@%s/wd/hub",
+                    System.getProperty("selenoid.login", "user1"),
+                    System.getProperty("selenoid.password", "1234"),
+                    remote
+            );
+        }
 
         ChromeOptions options = new ChromeOptions();
         options.addArguments(
@@ -44,10 +73,16 @@ public class TestBase {
         options.setExperimentalOption("excludeSwitches", List.of("enable-automation"));
         options.setExperimentalOption("useAutomationExtension", false);
 
-        capabilities.setCapability(ChromeOptions.CAPABILITY, options);
+        options.setPageLoadStrategy(PageLoadStrategy.EAGER);
 
-        Configuration.browserCapabilities = capabilities;
+        if (!remote.isBlank()) {
+            options.setCapability("selenoid:options", Map.of(
+                    "enableVNC", true,
+                    "enableVideo", true
+            ));
+        }
 
+        Configuration.browserCapabilities = options;
     }
 
 
